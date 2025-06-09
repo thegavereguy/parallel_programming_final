65e1d837829281a22d5fe0e5eea71bcda9a7c946
thegavereguy <dallacia.enrico@protonmail.com>
2025-06-09 20:30:28 +0200

    added weak scaling test cases

--- include/lib/common.h
+++ include/lib/common.h
@@ -9,6 +9,13 @@ struct Conditions {
   const int n_x;
   const int n_t;
 };
+
+const double L_PER_UNIT = 1.0;
+const double ALPHA      = 0.01;
+const double T_FINAL    = 0.1;
+const int NX_INTERVALS  = 2 << 13;  // Numero di intervalli per unità
+const int NT            = 1000;
+
 void initialize_array(float* array, int size);
 
 const float expected[16] = {100.0000,  50.5701,   19.2164,   5.61374,
@@ -19,8 +26,6 @@ const float expected[16] = {100.0000,  50.5701,   19.2164,   5.61374,
 const std::vector<std::pair<Conditions, std::string>> target_cases = {
     {{1, .1, 1.0, 50, 100}, "Stability_Test_Large_dt"},
     {{1, .1, 1.0, 50, 50}, "Stability_Test_Very_Large_dt"},
-    {{1, .05, 1.0, 100, 200}, "Standard_Diffusion"},
-    {{1, .05, 1.0, 200, 400}, "Fine_Spatial_Resolution"},
     {{1, 1, 1.0, 100, 1000}, "Long_Time_Simulation"},
     {{1, .1, 1.0, 50, 2000}, "Fine_Time_Stepping"},
     {{1, .1, 1.0, 5000, 2000}, "Very_High_Spatial_Resolution"},
@@ -31,6 +36,31 @@ const std::vector<std::pair<Conditions, std::string>> target_cases = {
     {{1, .05, 10.0, 8192, 10000}, "Very_Large_Problem_1"},
     {{1, .05, 10.0, 16384, 20000}, "Very_Large_Problem_2"},
     {{1, .05, 10.0, 32768, 40000}, "Very_Large_Problem_3"},
-    {{1, 0.5, 1.0, 100, 100}, "High_Alpha_Explicit_Stability_Limit"},
-    {{1, 1.0, 1.0, 100, 100}, "Very_High_Alpha_Implicit_Advantage"},
+};
+
+const std::vector<std::pair<Conditions, std::string>> weak_scaling_cases = {
+    {{L_PER_UNIT * 1, ALPHA, T_FINAL, (NX_INTERVALS * 1) + 1, NT},
+     "WeakScaling_P1_Nx" + std::to_string((NX_INTERVALS * 1) + 1) + "_L" +
+         std::to_string(L_PER_UNIT * 1)},
+    {{L_PER_UNIT * 2, ALPHA, T_FINAL, (NX_INTERVALS * 2) + 1, NT},
+     "WeakScaling_P2_Nx" + std::to_string((NX_INTERVALS * 2) + 1) + "_L" +
+         std::to_string(L_PER_UNIT * 2)},
+    {{L_PER_UNIT * 4, ALPHA, T_FINAL, (NX_INTERVALS * 4) + 1, NT},
+     "WeakScaling_P4_Nx" + std::to_string((NX_INTERVALS * 4) + 1) + "_L" +
+         std::to_string(L_PER_UNIT * 4)},
+    {{L_PER_UNIT * 8, ALPHA, T_FINAL, (NX_INTERVALS * 8) + 1, NT},
+     "WeakScaling_P8_Nx" + std::to_string((NX_INTERVALS * 8) + 1) + "_L" +
+         std::to_string(L_PER_UNIT * 8)},
+    {{L_PER_UNIT * 16, ALPHA, T_FINAL, (NX_INTERVALS * 16) + 1, NT},
+     "WeakScaling_P16_Nx" + std::to_string((NX_INTERVALS * 16) + 1) + "_L" +
+         std::to_string(L_PER_UNIT * 16)},
+    // {
+    //     {L_PER_UNIT_WS * 32,
+    //      ALPHA_CONST_WS,
+    //      T_FINAL_CONST_WS,
+    //      (NX_INTERVALS_PER_UNIT_WS * 32) + 1,
+    //      NT_CONST_WS},
+    //     "WeakScaling_P32_Nx" + std::to_string((NX_INTERVALS_PER_UNIT_WS * 32)
+    //     + 1) + "_L" + std::to_string(L_PER_UNIT_WS * 32)
+    // }
 };
