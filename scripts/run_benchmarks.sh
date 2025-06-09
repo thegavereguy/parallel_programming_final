b3bfa12c6ee7fb3b0a308ba71b41b29086a45c4a
thegavereguy <dallacia.enrico@protonmail.com>
2025-06-09 21:53:38 +0200

    major adaption of benchmark script

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -1,62 +1,34 @@
 #!/bin/bash
 
-cpu_benches=(   "cpu_par4_all" );
-gpu_benches=(   "gpu1024");
+omp_tests=("seq_ex" "seq_im")
 
 if [ -z "$BENCHMARK_SAMPLES" ]; then
-	export BENCHMARK_SAMPLES=5;
+	export BENCHMARK_SAMPLES=5
 fi
 
 if [ -z "$BENCHMARK_CONFIDENCE_INTERVAL" ]; then
-	export BENCHMARK_CONFIDENCE_INTERVAL=0.30;
+	export BENCHMARK_CONFIDENCE_INTERVAL=0.30
 fi
 
-if [ -z "$BUILD_VULKAN" ]; then
-	export BUILD_VULKAN=false
-fi
-
-echo "Running $BENCHMARK_SAMPLES samples per benchmark with $BENCHMARK_CONFIDENCE_INTERVAL confidence interval";
+echo "Running $BENCHMARK_SAMPLES samples per benchmark with $BENCHMARK_CONFIDENCE_INTERVAL confidence interval"
 # go to folder /build/tests relative to the project root
+if [ -z "$PROJECT_ROOT" ]; then
+	PROJECT_ROOT=$(git rev-parse --show-toplevel)
+fi
+
 cd $PROJECT_ROOT/build/tests
 
-for i in ${cpu_benches[@]}; do
-		echo "Running benchmark for [$i]";
-		eval "./cpu_bench" \"[$i]\" "-r csv" "--benchmark-samples=$BENCHMARK_SAMPLES" "--benchmark-confidence-interval=$BENCHMARK_CONFIDENCE_INTERVAL" > "$PROJECT_ROOT/results/$i.csv";
+for i in ${omp_tests[@]}; do
+	echo "Running benchmark for [$i]"
+	eval "./cpu_bench" \"[$i]\" "-r csv" "--benchmark-samples=$BENCHMARK_SAMPLES" "--benchmark-confidence-interval=$BENCHMARK_CONFIDENCE_INTERVAL" >"$PROJECT_ROOT/results/$i.csv"
 done
 
-if [ ${BUILD_VULKAN} = true ]; then
-	for i in ${gpu_benches[@]}; do
-		echo "Running benchmark for [$i] ";
-		eval "./gpu_bench" \"[$i]\" "-r csv" "--benchmark-samples=$BENCHMARK_SAMPLES" "--benchmark-confidence-interval=$BENCHMARK_CONFIDENCE_INTERVAL" > "$PROJECT_ROOT/results/$i.csv";
-	done
-else
-	echo "Skipping Vulkan benchmarks";
-fi
+echo "Running MPI benchmarks"
 
-if [ ${BUILD_MPI} = true ]; then
-	echo "Running MPI benchmarks";
+n=(1 2 4)
 
-	L=(1.0 1.0 1.0 1.0 1.25 1.25 1.25 1.5)
-	alpha=(0.001 0.0015 0.002 0.0025 0.003 0.0035 0.004 0.0045)
-	t_final=(1.0 1.25 1.5 1.75 2.0 2.25 2.25 2.5)
-	n_x=(256 512 1024 2048 4096 8192 16384 32768)
-	n_t=(2000 65000 130000 195000 260000 325000 390000 455000)
-
-# L=(1.0 1.0 1.0 1.0 1.25 1.25 1.25 1.25)
-# alpha=(0.001 0.001 0.001 0.001 0.001 0.001 0.001 0.001)
-# t_final=(1.0 1.0 1.5 1.5 2.0 2.0 2.5 2.5)
-# n_x=(1024 4096 16384 65536 262144 524288 1048576 2097152)
-# n_t=(100000 100000 150000 150000 200000 200000 250000 250000)
-
-	n=( 2 4 8  )
-
-	for iter in "${n[@]}"; do
-		echo "Running MPI benchmark for $iter processes";
-		echo "NX,NT,MEAN,MINT,MAXT,ITER" > "$PROJECT_ROOT/results/mpi_$iter.csv"
-		for i in "${!L[@]}"; do
-			../apps/mpi  ${L[i]}  ${alpha[i]}  ${t_final[i]} ${n_x[i]} ${n_t[i]} $iter >> "$PROJECT_ROOT/results/mpi_$iter.csv"
-		done
-	done
-	else
-	echo "Skipping MPI benchmarks";
-fi
+for iter in "${n[@]}"; do
+	echo "Running MPI benchmark for $iter processes"
+	mpirun -np $iter ../apps/mpi >>"$PROJECT_ROOT/results/mpi_$iter.csv"
+	# valutare se si riesce a spostare l'eseguibile in /build/tests
+done
